fb57ad8949177a65f194b461cd97baf9f6b28774
zhaoxuejiao <tinklexjane@gmail.com>
2017-05-25 14:14:50 +0800

    add destroy message info

--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -6,6 +6,8 @@ class Api::V1::TeachersController < ApiController
 
   def destory
     @teacher = Teacher.find(params[:id])
+    @message = "删除teacher:#{@teacher.id} 与其所带课程的全部信息"
     @teacher.destroy
+    render json: { message: @message }
   end
 end
