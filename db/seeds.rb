2028b127ba17bca37e5d576e2be262ef0fe607c0
zhaoxuejiao <tinklexjane@gmail.com>
2017-05-25 13:45:57 +0800

    add seeds for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Teacher.delete_all
+Student.delete_all
+Course.delete_all
+
+puts 'create teachers'
+teacher1 = Teacher.create!(name: 'Mr Li')
+teacher2 = Teacher.create!(name: 'Mrs Xd')
+
+puts 'create students'
+student1 = Student.create!(name: 'Lily')
+student2 = Student.create!(name: 'Lucy')
+student3 = Student.create!(name: 'Jane')
+
+puts 'create courses for student1'
+course1 = teacher1.courses.create!(name: 'Math')
+course2 = teacher1.courses.create!(name: 'English')
+course3 = teacher2.courses.create!(name: 'Physics')
+
+puts 'student has many courses'
+course1.students << student1
+course1.students << student2
+course2.students << student3
+course3.students << student3
